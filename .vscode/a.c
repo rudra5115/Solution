e170fd844b1af956ffb2af45525770535b01e8f7
rudra5115 <rudrahpatel5115.com>
2024-04-10 16:41:32 +0530

    test

---
+++ .vscode/a.c
@@ -0,0 +1,79 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int age;
+//     printf("enter age");
+//     scanf("%d",&age);
+//     age>=21?printf("marry now"):printf("marry after sometime");
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main() {
+//     int day;
+//     printf("Enter day:");
+//     scanf("%d", &day);
+//     switch (day) {
+//         case 1:
+//             printf("Sunday");
+//             break;
+//         case 2:
+//             printf("Monday");
+//             break;
+//         case 3:
+//             printf("Tuesday");
+//             break;
+//         case 4:
+//             printf("Wednesday");
+//             break;
+//         case 5:
+//             printf("Thursday");
+//             break;
+//         case 6:
+//             printf("Friday");
+//             break;
+//         case 7:
+//             printf("Saturday");
+//             break;
+//         default:
+//             printf("Sorry");
+//             break;
+        
+//     }
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main(){
+//   int num;
+//   printf("enter number you want to check");
+//   scanf("%d",&num);
+//   if(num%2==0){
+//       printf("even");
+//   }
+//   else
+//   {
+//       printf("odd");
+//   }
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int sy;
+//     int ey;
+//     printf("enter start year");
+//     scanf("%d",&sy);
+//     printf("enter end year");
+//     scanf("%d",&ey);
+//     for(int i=sy;i<=ey;i++){
+//       if(i%4==0){
+//           printf("%d\n",i);
+//       }
+//     }
+// }
+
+
